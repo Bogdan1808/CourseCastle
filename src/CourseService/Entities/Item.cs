1d4ae5bc03c7615897e51d88c111c0b1cae49966
Bogdan1808 <bnpasca@gmail.com>
2024-10-24 19:25:14 +0300

    Initial commit

---
+++ src/CourseService/Entities/Item.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CourseService.Entities;
+
+[Table("Items")]
+public class Item
+{
+    public Guid Id { get; set; }
+    public string CourseTitle { get; set; }
+    public string Instructor { get; set; }
+    public string Description { get; set; }
+    public DateOnly DateCreated { get; set; }
+    public int Level { get; set; }
+    public TimeOnly Duration { get; set; }
+    public int CoursePrice { get; set; }
+    public float Rating { get; set; }
+    public string ImageUrl { get; set; }
+
+    //relations
+    public Course Course { get; set; }
+    public Guid CourseId { get; set; }
+}
