120b2fd76c20e4975ea2d3c2d09395294dc16481
Pasca Bogdan-Nicolae <bnpasca@gmail.com>
2024-11-04 16:01:44 +0200

    DeleteRequest

---
+++ RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using CourseService.DTOs;
+using CourseService.Entities;
+
+namespace CourseService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Course, CourseDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, CourseDto>();
+        CreateMap<CreateCourseDto, Course>()
+            .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateCourseDto, Item>();
+    }
+}
